8821cc6924ab517c0f085d5a4f28f0ecd54aea0a
nokk <milan.hommet@protonmail.com>
2024-02-22 19:39:38 +0100

    feat(qr): :sparkles: quest page and qr scanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
+import React, { useState } from "react";
+import "./globals.css";
+import Scanner from "./components/scanner";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
-export default function Home() {
+const App = () => {
+  const [showScanner, setShowScanner] = useState(false);
+
+  const handleCompleteQuest = () => {
+    setShowScanner(true);
+  };
+
   return (
     <div className="bg-white">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate px-6 pt-5 lg:px-8">
         <div
-          className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
           <div
@@ -16,29 +27,70 @@ export default function Home() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              A MDS project
-            </div>
+        <div className="flex justify-between">
+          <div className="flex items-center">
+            <Image
+              src="/img/icons/icon-512x512.png"
+              alt="Campus Quest Icon"
+              width={40}
+              height={50}
+              className="icon"
+            />
           </div>
+          <div className="flex items-center userbtn">
+            <UserButton />
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Campus Quest
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/quests"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-            </div>
+            <ul
+              role="list"
+              className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+            >
+              <li className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                <div className="flex w-full items-center justify-between space-x-6 p-6">
+                  <div className="flex-1 truncate">
+                    <div className="flex items-center space-x-3">
+                      <h3 className="truncate text-sm font-medium text-gray-900">
+                        Nom de la quete
+                      </h3>
+                    </div>
+                    <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-blue-600 ring-1 ring-inset ring-green-600/20">
+                      Completed
+                    </span>
+                    <p className="mt-1 truncate text-sm text-gray-500">
+                      Description
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="-mt-px flex divide-x divide-gray-200">
+                    <div className="-ml-px flex w-0 flex-1">
+                      <a
+                        href="tel:+1-202-555-0170"
+                        className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                        onClick={handleCompleteQuest}
+                      >
+                        Complete quest
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            {showScanner && <Scanner />}
           </div>
         </div>
         <div
@@ -56,4 +108,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default App;
