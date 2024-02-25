cd0c5da98f46063e0ba949491c27f6ef7b74b13e
milan144 <milan.hommet@protonmail.com>
2024-02-25 21:13:27 +0100

    fix: test fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... other options you like
-};
+   reactStrictMode: true,
+}
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig
