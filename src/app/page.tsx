328db1535bb4cc067706e6c7c97675a758720352
milan144 <milan.hommet@protonmail.com>
2024-02-25 16:56:56 +0100

    feat: adding loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ const App = () => {
               role="list"
               className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
             >
-              {Array.isArray(quests) &&
+              {Array.isArray(quests) && quests.length > 0 ? (
                 quests.map((quest) => (
                   <li
                     key={quest.code}
@@ -166,7 +166,14 @@ const App = () => {
                       </div>
                     </div>
                   </li>
-                ))}
+                ))
+              ) : (
+                <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+                  <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+                  <h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
+                  <p className="w-1/3 text-center text-white">The quests are loading, this may take a few seconds, please don't close this page.</p>
+                </div>
+              )}
             </ul>
           </div>
         </div>
