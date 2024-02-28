00318f688dc655cb4fe2cbe56e701b2b63ae4045
milan144 <milan.hommet@protonmail.com>
2024-02-28 19:11:12 +0100

    feat: making a version without location check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -182,12 +182,8 @@ const App = () => {
             <button
               className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
-              onClick={async () => {
-                if (await checkLocation()) {
+              onClick={ () => {
                   setShowModal(true);
-                } else {
-                  notifyLocation();
-                }
               }}
             >
               SCAN A QR CODE
